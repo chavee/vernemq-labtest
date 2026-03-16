10f4f81f8bb489e7b3c3bafce4f6c455d6bfb874
chavee <i@chavee.com>
2026-03-16 13:44:20 +0700

    update script

--- 02-start-container.sh
+++ 02-start-container.sh
@@ -6,5 +6,5 @@ docker compose up -d
 echo "⏳ Waiting for VerneMQ Original to be healthy..."
 sleep 10
 
-echo "📜 Loading custom Redis counter script..."
-docker compose exec vernemq-original vmq-admin script load path=/etc/vernemq/lua/auth.lua || true
+#echo "📜 Loading custom Redis counter script..."
+#docker compose exec vernemq-original vmq-admin script load path=/etc/vernemq/lua/auth.lua || true
